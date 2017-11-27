013b024d758339a284e5c418b57bafdca0c8c968
Katie-Loyd <katiecameronloyd@gmail.com>
2017-11-27 10:06:33 -0500

    Refactored for MVVM

--- js/map.js
+++ js/map.js
@@ -1,33 +1,55 @@
 // Model
-var map;
+var initialMarkers = [
+  {
+    title: "Old Salem",
+    lat: 36.087144,
+    lng: -80.242494
+  },
+  {
+    title: "Greensboro Science Center",
+    lat: 36.129938,
+    lng: -79.834127
+  },
+  {
+    title: "Elon University",
+    lat: 36.103408,
+    lng: -79.501255
+  },
+  {
+    title: "Haw River State Park",
+    lat: 36.250866,
+    lng: -79.756397
+  },
+  {
+    title: "Print Works Bistro",
+    lat: 36.088826,
+    lng: -79.821446
+  }
+]
+
+var Marker = function(loc) {
+  this.position = {lat: loc.lat, lng: loc.lng},
+  this.title = loc.title
+}
+
+// ViewModel
+var markers = ko.observableArray([]);
+
+initialMarkers.forEach(function(markerLocation){
+  markers.push(new Marker(markerLocation));
+});
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-      center: {lat: 36.105982, lng: -79.856529},
-      zoom: 11
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: 36.105982, lng: -79.856529},
+    zoom: 11
   });
-  var oldSalem = new google.maps.Marker({
-      position: {lat: 36.087144, lng: -80.242494},
-      map: map,
-      title: "Old Salem"
-      });
-  var scienceCenter = new google.maps.Marker({
-      position: {lat: 36.129938, lng: -79.834127},
-      map: map,
-      title: "Greensboro Science Center"
-      });
-  var elonUniversity = new google.maps.Marker({
-      position: {lat: 36.103408, lng: -79.501255},
-      map: map,
-      title: "Elon University"
-      });
-  var hawRiver = new google.maps.Marker({
-      position: {lat: 36.250866, lng: -79.756397},
-      map: map,
-      title: "Haw River State Park"
-      });
-  var printWorks = new google.maps.Marker({
-      position: {lat: 36.088826, lng: -79.821446},
-      map: map,
-      title: "Print Works Bistro"
-      });
+
+  initialMarkers.forEach(function(markerData) {
+    new google.maps.Marker({
+    position: {lat: markerData.lat, lng: markerData.lng},
+    map: map,
+    title: markerData.title
+    });
+  });
 };
