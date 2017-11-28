9195d771d9a360b48c32f583567b5e38cd5c7d21
Katie-Loyd <katiecameronloyd@gmail.com>
2017-11-28 18:59:52 -0500

    Reorganized for MVVM

--- js/map.js
+++ js/map.js
@@ -32,10 +32,47 @@ var initialMarkers = [
   }
 ]
 
+// Class for creating new marker list instances for each location
+var Marker = function(loc) {
+  this.position = {lat: loc.lat, lng: loc.lng},
+  this.title = loc.title
+};
+
+
+// *** ViewModels ***
+// ** ViewModel for map **
+// Initialize map with markers
+function initMap() {
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: {lat: 36.109034, lng: -79.859619},
+    zoom: 11
+  });
+
+  function getMarker(marker){
+    // Displays marker on map
+    var mark = new google.maps.Marker({
+      position: {lat: marker.lat, lng: marker.lng},
+      map: map,
+      title: marker.title
+    });
+
+    // Displays info window when marker is clicked
+    var info = new google.maps.InfoWindow({
+      content: infoContent(marker)
+    });
+    mark.addListener("click", function(){
+      info.open(map, mark);
+    });
+  };
+
+  for (var m=0; m<initialMarkers.length; m++){
+    getMarker(initialMarkers[m]);
+  };
+
+};
+
 // Get Flickr photo data for each location
 function getAjax(marker) {
-  // Set current location to the "marker" variable for easy reference
-  var marker = initialMarkers[m];
   // Set the Flickr api request url to search photos with the location name as a tag
   var flickrRequestUrl = "https://api.flickr.com/services/rest/?method=" +
     "flickr.photos.search&api_key=cd7a678487f7cec2b53ed11ba7a1de15&tags=" +
@@ -65,44 +102,6 @@ function getAjax(marker) {
   });
 };
 
-for (var m=0; m<initialMarkers.length; m++){
-  getAjax(initialMarkers[m]);
-};
-
-// Class for creating new marker list instances for each location
-var Marker = function(loc) {
-  this.position = {lat: loc.lat, lng: loc.lng},
-  this.title = loc.title
-};
-
-
-// *** ViewModels ***
-// ** ViewModel for map **
-// Initialize map with markers
-function initMap() {
-  var map = new google.maps.Map(document.getElementById("map"), {
-    center: {lat: 36.109034, lng: -79.859619},
-    zoom: 11
-  });
-
-  // Create marker and info window for each place in initialMarkers
-  initialMarkers.forEach(function(markerData) {
-    // Displays marker on map
-    var mark = new google.maps.Marker({
-      position: {lat: markerData.lat, lng: markerData.lng},
-      map: map,
-      title: markerData.title
-    });
-
-    // Displays info window when marker is clicked
-    var info = new google.maps.InfoWindow({
-      content: infoContent(markerData)
-    });
-    mark.addListener("click", function(){
-      info.open(map, mark);
-    });
-  });
-};
 
 // ** ViewModel for list **
 // Create observable array for displaying marker names in the sidebar list
@@ -115,15 +114,20 @@ initialMarkers.forEach(function(markerLocation){
 
 // *** View ***
 // ** View for map **
+// Generate photo links for each place
+for (var m=0; m<initialMarkers.length; m++){
+  getAjax(initialMarkers[m]);
+};
+
 // Generate HTML to render place link and Flickr photos in info window
 function infoContent(placeData){
   var content = "<a target=blank href='" + placeData.link + "'>" +
       "<h5>" + placeData.title + "</h5>" +
     "</a>" +
     "<div class='carousel'>"
-    var photoList = placeData.photos
-    if (photoList) {
-      photoList.forEach(function(photo){
+    console.log(placeData.photos);
+    if (placeData.photos) {
+      placeData.photos.forEach(function(photo){
         content += "<figure class='carousel-item'>" +
             "<img src='" + photo.thumbSource + "'>" +
             "<figcaption>" +
